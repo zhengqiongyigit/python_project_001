1a54902eb67054c0c854d340646c610e8e725fb7
win10 <2328464118@qq.com>
2020-06-21 16:17:05 +0800

    提交

---
+++ pythoncode/demo.py
@@ -0,0 +1,13 @@
+import fibo
+import work_day
+
+fib = fibo.fib
+
+fib(1000)
+
+fib2 = fibo.fib2
+
+print(fib2(100))
+
+work_day.day("Friday")
+
---
+++ pythoncode/work_day.py
@@ -0,0 +1,13 @@
+def day(work_day):
+    if work_day == "Monday":
+        print("今天是工作日的第一天")
+    elif work_day == "Tuesday":
+            print("今天是工作日的第二天")
+    elif work_day == "Wednesday":
+            print("今天是工作日的第三天")
+    elif work_day == "Thursday":
+            print("今天是工作日的第四天")
+    elif work_day == "Friday":
+            print("今天是工作日的最后一天，好开心")
+    else:
+        print("今天是休息日，不用上班，美美哒")
